31e4125eb526af709a9743f8a43d502e6fa5543c
Manoah Kinnaert <manoah.k@icloud.com>
2026-09-09 10:30:57 +0200

    Some adjustments

--- SmollCode/display.py
+++ SmollCode/display.py
@@ -2,11 +2,11 @@ from constants import *
 import os 
 import re
 
-def render_title_logo(filled: bool=True):
-    print(f"{BOLD}{RED}{LOGO_FILLED if filled else LOGO}{RESET}\n")
+def render_title_logo():
+    print(f"{BOLD}{RED}{LOGO}{RESET}\n")
 
 def render_separator():
-    print(f"{DIM}{'─' * min(os.get_terminal_size().columns, 80)}{RESET}")
+    print(f"{DIM}{'─' * min(os.get_terminal_size().columns, 90)}{RESET}")
 
 def render_markdown(markdown: str, other: str=""):
     print(other + re.sub(r"\*\*(.+?)\*\*", rf"{BOLD}\1{RESET}", markdown))
